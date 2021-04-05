17caae6599c2c3f58b1ae8d1444c36ec56fbff8f
Taka2401 <tkht2401@gmail.com>
2021-04-05 02:59:38 +0000

    submit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
 
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+
+  get 'following_user/:id' => 'users#following', as: 'following_user'
+  get 'followers_user/:id' => 'users#followers', as: 'followers_user'
+
+
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update]
@@ -10,6 +17,5 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-
 
 end
